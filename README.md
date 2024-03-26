4144bf2b32dc105dd3747aeca52667e0f7ac6c37
jia zhang <you@example.com>
2024-03-26 11:08:16 +0800

    update project

---
+++ README.md
@@ -0,0 +1,40 @@
+## rstudio-server-singularity
+Run rstudio-server in conda environment with singularity installed
+
+## Acknowledgements
+I adopted script from @grst's project `rstudio-server-conda` (https://github.com/grst/rstudio-server-conda)
+This script use @rocker's rstudio  singularity img (https://github.com/rocker-org/rocker)
+
+## install
+### install singularity
+#### with root privilege
+`https://docs.sylabs.io/guides/3.0/user-guide/installation.html`
+#### without root privilege
+`https://docs.sylabs.io/guides/3.5/admin-guide/installation.html#install-nonsetuid`
+
+also see: `https://blog.csdn.net/lazysnake666/article/details/122834329` (Chinese)
+
+1) clone this project and cd to project directory
+
+2) activate conda environment you want
+`conda activate env_name`
+
+3) run script
+```
+WORKDIRECTORY="/path/to/your/rproject/workdir/"
+PORT=8787 PASSWORD=PASSWORD ./run_singularity.sh #change the port number if your if your port is already occupied (for example, another rstudio server is running)
+```
+
+4) visit `http://your_server_ip:8787`
+
+Note: if your server is under a firewall, I recommend using SSH port forwarding.
+
+eg: 
+
+```
+# keep this ssh connect running throughout the entire session
+ssh user_name@your_server_ip -L 8787:127.0.0.1:8787
+```
+
+visit http://127.0.0.1:8787
+
---
+++ run_singularity.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# See also https://www.rocker-project.org/use/singularity/
+
+# Main parameters for the script with default values
+PORT=${PORT:-8787}
+USER=$(whoami)
+PASSWORD=${PASSWORD:-notsafe}
+TMPDIR=${TMPDIR:-tmp}
+#CONTAINER="rstudio_latest.sif"  # path to singularity container (will be automatically downloaded)
+CONTAINER="rstudio_4.2.sif"
+WORKDIRECTORY="${WORKDIRECTORY}-$HOME"
+
+# Set-up temporary paths
+RSTUDIO_TMP="${TMPDIR}/$(echo -n $CONDA_PREFIX | md5sum | awk '{print $1}')"
+mkdir -p $RSTUDIO_TMP/{run,var-lib-rstudio-server,local-share-rstudio}
+
+R_BIN=$CONDA_PREFIX/bin/R
+PY_BIN=$CONDA_PREFIX/bin/python
+
+if [ ! -f $CONTAINER ]; then
+	#singularity build --fakeroot $CONTAINER Singularity
+	singularity pull docker://rocker/rstudio:4.2
+fi
+
+if [ -z "$CONDA_PREFIX" ]; then
+  echo "Activate a conda env or specify \$CONDA_PREFIX"
+  exit 1
+fi
+
+echo "Starting rstudio service on port $PORT ..."
+singularity exec \
+	--bind $RSTUDIO_TMP/run:/run \
+	--bind $RSTUDIO_TMP/var-lib-rstudio-server:/var/lib/rstudio-server \
+	--bind /sys/fs/cgroup/:/sys/fs/cgroup/:ro \
+	--bind database.conf:/etc/rstudio/database.conf \
+	--bind rsession.conf:/etc/rstudio/rsession.conf \
+	--bind $RSTUDIO_TMP/local-share-rstudio:/home/rstudio/.local/share/rstudio \
+	--bind ${CONDA_PREFIX}:${CONDA_PREFIX} \
+	--bind $HOME/.config/rstudio:/home/rstudio/.config/rstudio \
+        `# add additional bind mount required for your use-case` \
+	--bind ${WORKDIRECTORY}:/data \
+	--env CONDA_PREFIX=$CONDA_PREFIX \
+	--env RSTUDIO_WHICH_R=$R_BIN \
+	--env RETICULATE_PYTHON=$PY_BIN \
+	--env PASSWORD=$PASSWORD \
+	--env PORT=$PORT \
+	--env USER=$USER \
+	$CONTAINER \
+	/usr/lib/rstudio-server/bin/rserver --auth-none=0 --auth-pam-helper-path=pam-helper --server-user=$(whoami) --www-address=127.0.0.1 --www-port=$PORT
+
+
